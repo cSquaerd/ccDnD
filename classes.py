1e45f124bf0ae44df660c439c769acc08f3a405f
Charles Patterson Cook <cook.char.p@gmail.com>
2025-06-27 22:46:50 -0400

    Added MonetaryValue class

--- classes.py
+++ classes.py
@@ -30,3 +30,17 @@ class DnDLevelExp(enum.IntEnum):
 	L19 = 305000
 	L20 = 355000
 
+class MonetaryValue:
+	def __init__(self, copper : int, silver : int = 0, gold : int = 0):
+		self.value = copper + 10 * silver + 100 * gold
+
+	@classmethod
+	def from_gold(class_, amount : float) -> "MonetaryValue":
+		in_copper = round(amount * 100)
+		in_silver = round(amount * 10)
+		return class_(in_copper % 10, in_silver % 10, int(amount))
+
+	def __str__(self) -> str:
+		return f"\u20B2{self.value / 100:.2f}"
+
+
