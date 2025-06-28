ff8b9f7933a2e10adcdd6cdc66e5b5860806db69
Charles Patterson Cook <cook.char.p@gmail.com>
2025-06-27 22:23:19 -0400

    Began defining enums

---
+++ classes.py
@@ -0,0 +1,32 @@
+import enum
+
+class DnDAbilityName(enum.StrEnum):
+	STR = "Strength"
+	DEX = "Dexterity"
+	CON = "Constitution"
+	INT = "Intelligence"
+	WIS = "Wisdom"
+	CHR = "Charisma"
+
+class DnDLevelExp(enum.IntEnum):
+	L01 = 0
+	L02 = 300
+	L03 = 900
+	L04 = 2700
+	L05 = 6500
+	L06 = 14000
+	L07 = 23000
+	L08 = 34000
+	L09 = 48000
+	L10 = 64000
+	L11 = 85000
+	L12 = 100000
+	L13 = 120000
+	L14 = 140000
+	L15 = 165000
+	L16 = 195000
+	L17 = 225000
+	L18 = 265000
+	L19 = 305000
+	L20 = 355000
+
